4eb0cfa08f24daecbd25bbe422fd1b0f09d175ce
Yutakkkkj <natan.m@escolar.ifrn.edu.br>
2024-08-27 14:20:27 -0300

    r&t

--- Meu Game 3D/Assets/scripts/player.cs
+++ Meu Game 3D/Assets/scripts/player.cs
@@ -7,11 +7,24 @@ public class player : MonoBehaviour
 {
     public int velocidade = 10;
     Rigidbody rb;
-    // Start is called before the first frame update
+    public int forcaPulo = 10;
+    public bool noChao;
+    
     void Start()
     {
         TryGetComponent(out rb);
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Chão")
+        {
+
+        noChao = true;
+        }
+    }
+
+
 
     // Update is called once per frame
     void Update()
@@ -20,7 +33,15 @@ public class player : MonoBehaviour
       float v = Input.GetAxis("Vertical");
       Vector3 direcao = new Vector3(h, 0, v);
       rb.AddForce(direcao * velocidade * Time.deltaTime, ForceMode.Impulse);
-      if (transform.position.y < -5)
+      
+      if(Input.GetKeyDown(KeyCode.Space) && noChao)
+      {
+          rb.AddForce(Vector3.up * forcaPulo, ForceMode.Impulse);
+          noChao = false;
+      }
+      
+      
+      if (transform.position.y < -5) 
       {
           SceneManager.LoadScene(SceneManager.GetActiveScene().name);
       }
